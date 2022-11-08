1110b404d1bc420a1167faa99c7cbdef3a619e25
Ausra <ausrab@wix.com>
2022-11-08 08:41:43 +0200

    added global themes

---
+++ src/utils/global-styles/colors.ts
@@ -0,0 +1,18 @@
+export const blue = {
+  100: "#2B64F5",
+  200: "#3E71F4",
+};
+
+export const red = {
+  100: "##FF7064",
+};
+
+export const neutral = {
+  100: "#ffffff",
+  200: "#F5F5F5",
+  300: "#F7F7F7",
+  400: "#B2B2B2",
+  500: "#CCCCCC",
+  600: "#7F7F7F",
+  700: "#000000",
+};
---
+++ src/utils/global-styles/themes.ts
@@ -0,0 +1,12 @@
+import { blue, neutral, red } from "./colors";
+import { primaryFont } from "./typography";
+
+export const defaultTheme = {
+  primaryColor: blue[100],
+  primaryFont: primaryFont,
+  status: {
+    errorColor: red[100],
+  },
+
+  borderColor: neutral[500],
+};
