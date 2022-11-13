df51a534b5efc62694af2a12f4827ff39232fd9a
Ausra <ausrab@wix.com>
2022-11-13 11:12:42 +0200

    added to form dropdownfield

--- src/features/companyForm/company/Company.tsx
+++ src/features/companyForm/company/Company.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import Card from "../../../components/card";
+import DropdownField from "../../../components/dropdownField";
 import InputField from "../../../components/inputField";
 import { COMPANY } from "./company.validation";
 
@@ -23,7 +24,14 @@ const Company: FunctionComponent<CompanyProps> = ({
       >
         <InputField name={COMPANY.CODE} label="Company code" />
         <InputField name={COMPANY.NAME} label="Company name" />
-        <InputField name={COMPANY.COUNTRY} label="Country of registration" />
+        <DropdownField
+          name={COMPANY.COUNTRY}
+          label="country"
+          options={[
+            { country: "lithuania", id: "lithuania1" },
+            { country: "USA", id: "usa1" },
+          ]}
+        />
       </Card>
     </>
   );
